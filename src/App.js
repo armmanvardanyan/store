172173b09b546a2b27089f92e073f54d7a3a4220
Arman <av3838@mail.ru>
2022-05-30 23:46:26 +0400

    Understanding Outlet , nested routing !important

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import { Route, Routes } from "react-router-dom";
 import Home from "./routes/home/home.component";
 
+
+const Shop = () => {
+    return <div>
+      Shop component
+    </div>
+}
+
 const App = () => {
   return (
     <Routes>
-        <Route path="/" element = {<Home/>} />
-
+        <Route  path="home" element = {<Home/>} >
+          <Route path="shop" element = {<Shop/>} />
+        </Route>
     </Routes>
   );
 };
--- src/routes/home/home.component.jsx
+++ src/routes/home/home.component.jsx
@@ -1,7 +1,11 @@
+import { Outlet } from "react-router-dom";
 import Categories from "../../components/categories/categories.component";
 
 const Home = () => {
-    return <Categories/>
+    return <>
+        <Categories/>
+        <Outlet/> {/** this is Child Shop Route  postition in Home component */}
+    </>
 }
 
 

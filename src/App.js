d979db84a15bfb8da57a60eea02caeb0229de780
Arman <av3838@mail.ru>
2022-06-02 23:24:39 +0400

    creating initial navigation and using index for react router !

--- src/App.js
+++ src/App.js
@@ -1,6 +1,16 @@
-import { Route, Routes } from "react-router-dom";
+import { Outlet, Route, Routes } from "react-router-dom";
 import Home from "./routes/home/home.component";
 
+const Navigation = () => {
+   return (
+     <div>
+          <div>
+            <h1>i am the Navigation bar</h1>
+          </div>
+          <Outlet/>
+     </div>
+   )
+}
 
 const Shop = () => {
     return <div>
@@ -10,11 +20,14 @@ const Shop = () => {
 
 const App = () => {
   return (
+    <>
     <Routes>
-        <Route  path="home" element = {<Home/>} >
-          <Route path="shop" element = {<Shop/>} />
-        </Route>
+      <Route  path="/" element = {<Navigation/>} >
+        <Route index  element = {<Home/>} /> {/** index means that Home component will match also for parent / path inside Navigation parent  */}
+        <Route  path="shop" element = {<Shop/>} />
+      </Route>
     </Routes>
+    </>
   );
 };
 

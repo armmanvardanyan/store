0c9d94dcef672671db113dd520d4cc94c0005ef8
Arman <av3838@mail.ru>
2022-06-04 21:18:32 +0400

    Authenticating With Firebase with google popup

--- src/routes/sign-in/signin.component.jsx
+++ src/routes/sign-in/signin.component.jsx
@@ -1,8 +1,18 @@
+import { auth, signInWithGooglePopup } from "../../utils/firebase/firebase.utils";
 
 const SignIn = () => {
+
+    const logGoogleUser = async () => {
+        const response = await signInWithGooglePopup();
+        console.log(response);
+    }
+
     return (
         <div>
             <h1>Signin Page</h1>
+            <button onClick={logGoogleUser}>
+                Sign In With Google Popup
+            </button>
         </div>
     )
 
---
+++ src/utils/firebase/firebase.utils.js
@@ -0,0 +1,26 @@
+import { initializeApp } from "firebase/app";
+import {
+  getAuth,
+  signInWithRedirect,
+  signInWithPopup,
+  GoogleAuthProvider,
+} from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCR9-dTqdK91DaGsOgZ-PeBhOESXSdNN3w",
+  authDomain: "store-db-c435e.firebaseapp.com",
+  projectId: "store-db-c435e",
+  storageBucket: "store-db-c435e.appspot.com",
+  messagingSenderId: "387451984277",
+  appId: "1:387451984277:web:e3ddbcacfb6583e6b23e9c",
+  measurementId: "G-M0ST86LDRG",
+};
+
+// Initialize Firebase
+const firebaseApp = initializeApp(firebaseConfig);
+
+const provider = new GoogleAuthProvider();
+provider.setCustomParameters({ prompt: "select_account" });
+
+export const auth = getAuth();
+export const signInWithGooglePopup = () => signInWithPopup(auth,provider);

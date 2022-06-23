e34160feec2c4c44161fbde2e954fa7ac4bb25c9
Arman <av3838@mail.ru>
2022-06-23 23:31:06 +0400

    getting our categories list from firestore with query and geDocs

--- src/contexts/products.context.js
+++ src/contexts/products.context.js
@@ -1,5 +1,5 @@
 import { createContext, useEffect, useState } from "react";
-import { addCollectionAndDocuments } from "../utils/firebase/firebase.utils";
+import { getCategoriesAndDocuments } from "../utils/firebase/firebase.utils";
 
 export const ProductContext = createContext({
     products: []
@@ -7,6 +7,15 @@ export const ProductContext = createContext({
 
 export const ProductsProvider = ({children}) => {
     const [products,setProducts] = useState([])
+    
+
+    useEffect( () => {
+        const getCategoriesMap = async () => {
+            const categoryMap = await getCategoriesAndDocuments();
+            console.log(categoryMap);
+        }
+        getCategoriesMap()
+    },[])
 
 
     const value = {products}

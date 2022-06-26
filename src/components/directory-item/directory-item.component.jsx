9fc3c0d175daded6510164c73c608273bb03044e
Arman <av3838@mail.ru>
2022-06-26 17:30:12 +0400

    using programmatically navigating in home pgae for categories

--- src/components/directory-item/directory-item.component.jsx
+++ src/components/directory-item/directory-item.component.jsx
@@ -1,11 +1,17 @@
 import {DirectoryItemContainer,BackgroundImage,DirectoryBodyContainer} from './directory-item.style.jsx';
+import {useNavigate} from 'react-router-dom'
 
 const DirectoryItem = ({category}) => {
+
+  const {title,imageUrl,route} = category
+  const navigate = useNavigate();
+  
+  const onNavigateHandler = () => navigate(route)
     return (
-        <DirectoryItemContainer>
-          <BackgroundImage imageUrl = {category.imageUrl} />
+        <DirectoryItemContainer onClick={onNavigateHandler}>
+          <BackgroundImage imageUrl = {imageUrl} />
           <DirectoryBodyContainer>
-            <h2>{category.title}</h2>
+            <h2>{title}</h2>
             <p>Shop now</p>
           </DirectoryBodyContainer>
         </DirectoryItemContainer>
